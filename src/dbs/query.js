ed779f90c948bd28d6e5e9c792907d4990de846b
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-13 09:41:54 +0100

    general reframing of whole project

---
+++ src/dbs/query.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+/* eslint-disable arrow-body-style */
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: true,
+});
+
+pool.connect();
+
+const query = (text, params) => {
+  return new Promise((resolve, reject) => {
+    pool.query(text, params)
+      .then((res) => {
+        console.log(res);
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+module.exports = {
+  query,
+  pool,
+};

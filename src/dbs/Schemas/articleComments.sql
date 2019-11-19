f6ea66115c7fc055fed3a4a7badbf044be037b7f
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-20 00:08:43 +0100

    remove unwated sql scripts

--- src/dbs/Schemas/articleComments.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-      comments(
-        id SERIAL PRIMARY KEY,
-        userId INT NOT NULL,
-        comment TEXT NOT NULL,
-        articleID INTEGER NOT NULL,
-        post_date TIMESTAMP,
-        FOREIGN KEY (articleid) REFERENCES articles(id)
-      )
--- src/dbs/Schemas/gifComments.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-      gifComments(
-        id SERIAL PRIMARY KEY,
-        userid INT NOT NULL,
-        comment TEXT NOT NULL,
-        gifID INTEGER NOT NULL,
-        post_date TIMESTAMP,
-        FOREIGN KEY (gifid) REFERENCES gifs(id)
-      )

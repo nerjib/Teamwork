e9f7cbd13033fb2275dd30410b9fdcb50afaa22c
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-29 20:07:34 +0100

    cors

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const Auth = require('./src/middlewares/auth');
 
 
 const app = express();
+app.use(cors());
+
 http.createServer(app);
 dotenv.config();
 
@@ -38,15 +40,14 @@ app.use((req, res, next) => {
   next();
 });
 
-*/
+*
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-
-app.use(cors());
+*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());

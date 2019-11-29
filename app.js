f382518fefbb7d02313311f62036aad3c02654d2
kabirnjib0@gmail.com <kabirnajib0@gmail.com>
2019-11-29 19:14:26 +0100

    cors

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ http.createServer(app);
 dotenv.config();
 
 // HANDLING CORS ERRORS
+/*
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
@@ -37,14 +38,14 @@ app.use((req, res, next) => {
   next();
 });
 
-/*
+*/
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-*/
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
